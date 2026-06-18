852dcc73846079e93a08fc5b3b60dce1b342a5cb
RKSinghDU <208256921+RKSinghDU@users.noreply.github.com>
2026-06-18 08:39:30 +0530

    Update linkedinPosts.ts

--- src/content/linkedinPosts.ts
+++ src/content/linkedinPosts.ts
@@ -11,6 +11,7 @@ export interface LinkedInPost {
  *   4. Paste it as a new entry below. `caption` is optional.
  */
 export const LINKEDIN_POSTS: LinkedInPost[] = [
+  { url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7472457471220072448?collapsed=1' },
   { url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7463366561613918208?collapsed=1' },
   { url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7460547859545845765?collapsed=1' },
   { url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7423261493195988992' },
